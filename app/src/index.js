342c4a8df8d24733f2d1f65ebbbe2777a69204a0
Maxwell Haak <haakm7@gmail.com>
2020-10-17 21:37:15 -0700

    start front end

--- app/src/index.js
+++ app/src/index.js
@@ -1,17 +1,79 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+class Settings extends React.Component {
+  render() {
+    return (
+      <div className="radios">
+        <form>
+          <div className="form-check">
+            <label>
+              <input
+                type="radio"
+                name="react-tips"
+                value="option1"
+                checked={true}
+                className="form-check-input"
+              />
+              Option 1
+            </label>
+          </div>
+
+          <div className="form-check">
+            <label>
+              <input
+                type="radio"
+                name="react-tips"
+                value="option2"
+                className="form-check-input"
+              />
+              Option 2
+            </label>
+          </div>
+
+          <div className="form-check">
+            <label>
+              <input
+                type="radio"
+                name="react-tips"
+                value="option3"
+                className="form-check-input"
+              />
+              Option 3
+            </label>
+          </div>
+
+          <div className="form-group">
+            <button className="btn btn-primary mt-2" type="submit">
+              Save
+            </button>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>
+          Posture?
+        </h1>
+        <h2>
+          The home workplace health manager
+        </h2>
+        <div>
+          <Settings/>
+        </div>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
