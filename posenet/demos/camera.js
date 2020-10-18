a320ca3a373e1b6f027eb952771f6b77b2b6573e
vrita <dirtfield101@gmail.com>
2020-10-18 02:43:56 -0700

    alert msg

--- posenet/demos/camera.js
+++ posenet/demos/camera.js
@@ -146,7 +146,7 @@ async function findPoses(video, aves, maxlen) {
       postureHistory = postureHistory.concat(posturePeriod)
       
       if (posturePeriod.length >= maxlen) {
-        // TODO: Trigger alert
+        alert("Bad boy");
         console.log("Bad boy")
       } else {
         console.log("Good posture")
