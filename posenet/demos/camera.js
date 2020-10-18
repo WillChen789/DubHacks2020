3202c54ee5f4c3450c23af0e71bb479233c3a981
RishiChandra1 <rishi2000chandra@gmail.com>
2020-10-18 00:11:27 -0500

    added calibrate function

--- posenet/demos/camera.js
+++ posenet/demos/camera.js
@@ -110,6 +110,88 @@ async function findPoses(video) {
   }
 }
 
+async function calibrate() {
+  let timer  = 10000;
+  let ReyeX_Ave = 0;
+  let ReyeY_Ave = 0;
+  let LeyeX_Ave = 0;
+  let LeyeY_Ave = 0;
+  let RshoulderX_Ave = 0;
+  let RshoulderY_Ave = 0;
+  let LshoulderX_Ave = 0;
+  let LshoulderY_Ave = 0;
+  while (timer > 0) {
+    const posenet = require('@tensorflow-models/posenet');
+
+    async function estimatePoseOnImage(video) {
+      // load the posenet model from a checkpoint
+      const net = await posenet.load();
+
+      const pose = await net.estimateSinglePose(video, {
+        flipHorizontal: false
+      });
+      return pose;
+    }
+
+    const imageElement = document.getElementById('cat');
+
+    const pose = await estimatePoseOnImage(video);
+    const arr = pose["keypoints"];
+
+    var x_Reye = arr[2]["position"]["x"];
+    var y_Reye = arr[2]["position"]["y"];
+
+    var x_Leye = arr[1]["position"]["x"];
+    var y_Leye = arr[1]["position"]["y"];
+   
+    var x_Rshoulder = arr[6]["position"]["x"];
+    var y_Rshoulder = arr[6]["position"]["y"];
+
+    var x_Lshoulder = arr[5]["position"]["x"];
+    var y_Lshoulder = arr[5]["position"]["y"];
+
+    ReyeX_Ave += x_Reye;
+    ReyeY_Ave += y_Reye;
+    LeyeX_Ave += x_Leye;
+    LeyeY_Ave += y_Leye;
+    RshoulderX_Ave += x_Rshoulder;
+    RshoulderY_Ave += y_Rshoulder;
+    LshoulderX_Ave += x_Lshoulder;
+    LshoulderY_Ave += y_Lshoulder;
+
+    poseList.push(pose);
+    console.log(pose);
+    //console.log(poseList); // output the list of poses for debugging
+    console.log(x_Lshoulder);
+    console.log(y_Lshoulder);
+
+    timer -= 1000
+    await sleep(1000); // wait 5 seconds before logging next frame
+  }
+
+  ReyeX_Ave /= 10;
+  ReyeY_Ave /= 10;
+  LeyeX_Ave /= 10;
+  LeyeY_Ave /= 10;
+  
+  RshoulderX_Ave /= 10;
+  RshoulderY_Ave /= 10;
+  LshoulderX_Ave /= 10;
+  LshoulderY_Ave /= 10;
+
+  console.log(ReyeX_Ave);
+  console.log(ReyeY_Ave);
+
+  console.log(LeyeX_Ave);
+  console.log(LeyeY_Ave);
+
+  console.log(RshoulderX_Ave);
+  console.log(RshoulderY_Ave);
+
+  console.log(LshoulderX_Ave);
+  console.log(LshoulderY_Ave);
+}
+
 /**
  * Kicks off the demo by loading the posenet model, finding and loading
  * available camera devices, and setting off the findPoses function.
@@ -127,7 +209,7 @@ export async function bindPage() {
     throw e;
   }
 
-  findPoses(video);
+  calibrate(video);
 }
 
 navigator.getUserMedia = navigator.getUserMedia ||
