72f6f8830376b43504a962c7e7db7fad3be96a2f
vrita <dirtfield101@gmail.com>
2020-10-18 07:24:10 -0700

    website changes

--- app/src/Bottom_buttons.js
+++ app/src/Bottom_buttons.js
@@ -4,7 +4,7 @@ import './Bottom_buttons.css';
 
 function Buttons(props) {
     return (
-        <div>
+        <div className='buttonW'>
             <button
             id="calButton"
             value="calibrateButton"
